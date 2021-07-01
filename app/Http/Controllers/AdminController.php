86ef64780bb442ddfd15abd00900c210e0fafaf4
thunder <xthunder81@rocketmail.com>
2021-07-01 17:57:41 +0700

    add landing theme and modif

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\pegawai;
+use App\detail_pegawai;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -17,6 +18,48 @@ class AdminController extends Controller
     {
         //
         return view ('admin.home');
+    }
+
+    public function pegawaiView ()
+    {
+        $pegawai = pegawai::get();
+        return view('admin.pegawai', compact('pegawai'));
+    }
+
+    public function pegawaiCreate  ()
+    {
+        return view('admin.pegawai.create');
+    }
+
+    public function pegawaiStore (Request $request)
+    {
+        $request->validate([
+            'username' => 'required|min:6',
+            'password' => 'required|min:6',
+            'nama_pegawai' => 'required',
+            'jenis_kelamin' => 'required',
+            'tanggal_lahir' => 'required',
+            'tanggal_masuk' => 'required',
+            'kelurahan_pegawai' => 'required',
+            'kecamatan_pegawai' => 'required',
+            'kabupaten_pegawai' => 'required',
+            'provinsi_pegawai' => 'required',
+            'telpon_pegawai' => 'required',
+        ]);
+
+        pegawai::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+            'nama_pegawai' => ucwords(strtolower($request->nama_pegawai)),
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'tanggal_masuk' => $request->tanggal_masuk,
+            'kelurahan_pegawai' => $request->kelurahan_pegawai,
+            'kecamatan_pegawai' => $request->kecamatan_pegawai,
+            'kabupaten_pegawai' => $request->kabupaten_pegawai,
+            'provinsi_pegawai' => $request->provinsi_pegawai,
+            'telpon_pegawai' => $request->telpon_pegawai,
+        ]);
     }
 
     /**
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -159,8 +159,8 @@
 
             <!-- Brand Logo -->
             <a href="#" class="brand-link">
-                <img src="{{ asset ('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ asset ('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">BengkelHape.com</span>
             </a>
 
@@ -170,10 +170,11 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ asset ('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset ('dist/img/user2-160x160.jpg') }}"
+                            class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ \Auth::user()->nama_pegawai}}</a>
+                        <a href="#" class="d-block">{{ \Auth::user()->nama_pegawai }}</a>
                     </div>
                 </div>
 
@@ -209,11 +210,26 @@
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <i class="nav-icon far fa-address-card"></i>
                                 <p>
-                                    Pegawai
+                                    Master Pegawai
+                                    <i class="right fas fa-angle-left"></i>
                                 </p>
                             </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="./index.html" class="nav-link">
+                                        <i class="far fa-user nav-icon"></i>
+                                        <p>Pegawai</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="./index2.html" class="nav-link">
+                                        <i class="far fa-id-badge nav-icon"></i>
+                                        <p>Detail Pegawai</p>
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
